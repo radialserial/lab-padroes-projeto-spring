9de40656c59d791433157b13da9ea3e58cb13cb7
Natanael Alves Fernandes <natanael.alves.fernandes@gmail.com>
2024-04-10 17:59:36 -0300

    feat: add model

---
+++ src/main/java/com/radialserial/labpadroesprojetospring/model/EnderecoRepository.java
@@ -0,0 +1,8 @@
+package com.radialserial.labpadroesprojetospring.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends CrudRepository<Endereco, String> {
+}
